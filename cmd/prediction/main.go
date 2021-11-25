f7a33acab7b3688fbe83fb74fc96d79a8c2dbdd0
biswapcom <79948171+biswapcom@users.noreply.github.com>
2021-11-25 15:05:46 +0200

    Update main.go

--- cmd/prediction/main.go
+++ cmd/prediction/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/streamingfast/sparkle/cli"
 	"github.com/streamingfast/sparkle/subgraph"
-	"github.com/streamingfast/sparkle-pancakeswap/prediction"
+	"github.com/biswapcom/sparkle-biswap/prediction"
 )
 
 func main() {
