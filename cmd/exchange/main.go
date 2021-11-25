b7273594a8590f13db113c49fb1c53aae367d6e3
biswapcom <79948171+biswapcom@users.noreply.github.com>
2021-11-25 14:58:49 +0200

    Update main.go

--- cmd/exchange/main.go
+++ cmd/exchange/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/streamingfast/sparkle/cli"
 	"github.com/streamingfast/sparkle/subgraph"
-	"github.com/streamingfast/sparkle-pancakeswap/exchange"
+	"github.com/biswapcom/sparkle-biswap/exchange"
 )
 
 func main() {
